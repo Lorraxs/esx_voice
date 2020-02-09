659c0ba0ce84428ed7cc1bf59d54acf633beb8c8
Lorraxs <56058923+Lorraxs@users.noreply.github.com>
2020-02-09 23:13:37 +0700

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX Voice Controller'
+
+version '1.1.0'
+
+client_scripts {
+	'locales/br.lua',
+	'locales/en.lua',
+	'locales/fi.lua',
+	'locales/fr.lua',
+	'locales/pl.lua',
+	'locales/sv.lua',
+	'locales/ko.lua',
+	'config.lua',
+	'client/main.lua'
+}
